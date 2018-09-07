a77befd3a87645d4166c9fe843d9eae3037d3924
unknown <development.4reg.2qt3@gmail.com>
2018-09-08 06:35:13 +0900

    fix index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ server.on('request', (req, res) => {
             const webhookEventObj = JSON.parse(body);
 
             const data = {
-                replyToken: webhookEventObj.replyToken,
+                replyToken: webhookEventObj.events[0].replyToken,
                 messages: [
                     {
                         type: 'text',
