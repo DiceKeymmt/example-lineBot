3ec25ee90d9616322d5b539ab98ffa23656bf057
unknown <development.4reg.2qt3@gmail.com>
2018-09-08 04:39:30 +0900

    fix index.js

--- index.js
+++ index.js
@@ -32,48 +32,5 @@ const data = {
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    console.log(req.headers)
-    let rowData = '';
-
-    req.on('data', chunk => {
-        rowData += chunk;
-    })
-
-    req.on('end', () => {
-        if (!req.headers['x-line-signature']) {
-            res.writeHead(404, {
-                'Content-Type': 'text/plain'
-            });
-            res.end(`404 Not Found
-お探しのページは見つかりません。`)
-        } else {
-            const signature = crypto.createHmac('SHA256', config.channelSecret).update(rowData).digest('base64')
-
-            if (req.headers['x-line-signature'] === signature) {
-                data.replyToken = JSON.parse(rowData).events[0].replyToken;
-                
-                const req = https.request(options, res => {
-                    let requestBody = '';
-                    res.on('data', chunk => {
-                        requestBody += chunk;
-                    });
-
-                    res.on('end', () => {
-                        console.log(requestBody);
-                    });
-                });
-
-                req.on('error', err => {
-                    console.log(`https.request error
-${err}`);
-                });
-                console.log(data);
-                const d = JSON.stringify(data);
-                console.log(typeof d)
-                req.write(d);
-                req.end()
-                res.end('owata')
-            }
-        }
-    })
-}).listen(process.env.PORT || 8080);
+    console.log(req.url,req.method)
+}).listen(process.env.PORT||8080)
