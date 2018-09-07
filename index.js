0435a225844d59cdec76360bd2cc24c6a0259c45
unknown <development.4reg.2qt3@gmail.com>
2018-09-08 04:50:22 +0900

    fix index.js

--- index.js
+++ index.js
@@ -32,5 +32,42 @@ const data = {
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    console.log(req.url,req.method)
+    if (req.url !== '/webhook' || req.method !== 'POST') {
+        res.writeHead(404, {'Content-Type':'text/plain'})
+        res.end(`404
+Not Found`)
+    }
+
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk;
+    });
+
+    req.on('end', () => {
+        if (body === '') {
+            console.log('bodyが空です。');
+            return
+        }
+
+        data.replyToken = JSON.parse(body).events[0].replyToken;
+        const d = JSON.stringify(data);
+
+        const _req = https.request(options, res => {
+            res.on('data', chunk => {});
+            
+            res.on('end', () => {});
+        });
+
+        _req.on('error', err => {
+            console.log(err)
+        });
+
+        _req.write(d)
+
+        _req.end()
+
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('success');
+        
+    })
 }).listen(process.env.PORT||8080)
