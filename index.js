ac4f14bfe1d41294927e49429c9106b416dbe935
unknown <development.4reg.2qt3@gmail.com>
2018-09-09 04:29:25 +0900

    fix index.js

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ server.on('request', (req, res) => {
                         }
 
                     case 'location':
-                        const req = client(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3`);
-                        req.then( data => {
+                        client(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3`)
+                        .then( data => {
                             return {
                                 replyToken: webhookEventObj.events[0].replyToken,
                                 messages: [{
@@ -68,8 +68,7 @@ server.on('request', (req, res) => {
                                 }]
                             }
                         })
-
-                        break;
+                        console.log('promise end')
                     default:
                         return {
                             replyToken: webhookEventObj.events[0].replyToken,
