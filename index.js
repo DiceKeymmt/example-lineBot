95705b8a6337acbec1027371fde8f65ef2931542
unknown <development.4reg.2qt3@gmail.com>
2018-09-09 05:14:17 +0900

    fix index.js

--- index.js
+++ index.js
@@ -47,40 +47,41 @@ server.on('request', (req, res) => {
             const webhookEventObj = JSON.parse(body);
 
             const data = (() => {
-                switch (webhookEventObj.events[0].message.type) {
-                    case 'text':
-                        return {
-                            replyToken: webhookEventObj.events[0].replyToken,
-                            messages: [{
-                                type: 'text',
-                                text: webhookEventObj.events[0].message.text
-                            }]
-                        }
-
-                    case 'location':
-                        client(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3`)
+                if (webhookEventObj.events[0].message.type === 'text') {
+                    return {
+                        replyToken: webhookEventObj.events[0].replyToken,
+                        messages: [{
+                            type: 'text',
+                            text: webhookEventObj.events[0].message.text
+                        }]
+                    } 
+                } else if (webhookEventObj.events[0].message.type === 'location') {
+                    client(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3`)
                         .then( data => {
-                            console.log(data.results.shop[0].name)
+                            return {
+                                replyToken: webhookEventObj.events[0].replyToken,
+                                messages: [{
+                                    type: 'text',
+                                    text: data.results.shop[0].name
+                                },
+                                {
+                                    type: 'text',
+                                    text: data.results.shop[0].address
+                                }
+                            ]
+                            } 
                         })
-                        return {
-                            replyToken: webhookEventObj.events[0].replyToken,
-                            messages: [{
-                                type: 'text',
-                                text: 'location'
-                            }]
-                        }
-
-                    default:
-                        return {
-                            replyToken: webhookEventObj.events[0].replyToken,
-                            messages: [{
-                                type: 'text',
-                                text: '値が無効です。'
-                            }]
-                        }
+                } else {
+                    return {
+                        replyToken: webhookEventObj.events[0].replyToken,
+                        messages: [{
+                            type: 'text',
+                            text: '値が無効です。'
+                        }]
+                    }
                 }
             })();
-
+            
             options.headers['Content-Length'] = Buffer.byteLength(JSON.stringify(data));
 
             const req = https.request(options, res => {
