3afed1720d30ced8bca5404a4c8b4d374e61e8ec
unknown <development.4reg.2qt3@gmail.com>
2018-09-27 03:31:09 +0900

    mod module_createMessageObj.jsのリクエストurlの手直し

--- module_createMessageObj.js
+++ module_createMessageObj.js
@@ -3,7 +3,7 @@ const sendRequestToAPIServer = require('./module_sendRequestToAPIServer').sendRe
 const createMessageObj = (webhookEventObj, apiKey) => {
     switch (webhookEventObj.events[0].message.type) {
         case 'location':
-            sendRequestToAPIServer(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3&count=1`)
+            sendRequestToAPIServer(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3&count=1`)
             .then( v => {
                 console.log(v)
             })
