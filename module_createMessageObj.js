202bddd139e61e77851c27ba8f82782765e5adf5
unknown <development.4reg.2qt3@gmail.com>
2018-09-27 03:28:59 +0900

    mod module_createMessageObj.jsの仮引数名の手直し

--- module_createMessageObj.js
+++ module_createMessageObj.js
@@ -1,7 +1,7 @@
 const sendRequestToAPIServer = require('./module_sendRequestToAPIServer').sendRequestToAPIServer;
 
-const createMessageObj = (webhookObj, apiKey) => {
-    switch (webhookObj.events[0].message.type) {
+const createMessageObj = (webhookEventObj, apiKey) => {
+    switch (webhookEventObj.events[0].message.type) {
         case 'location':
             sendRequestToAPIServer(`http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3&count=1`)
             .then( v => {
