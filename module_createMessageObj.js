42ea35a2cc2f85efe879269c7a479f9cadb5f45e
unknown <development.4reg.2qt3@gmail.com>
2018-09-27 03:48:00 +0900

    mod module_createMessageObj.jsのapi返り値のテスト

--- module_createMessageObj.js
+++ module_createMessageObj.js
@@ -3,9 +3,12 @@ const sendRequestToAPIServer = require('./module_sendRequestToAPIServer').sendRe
 const createMessageObj = (webhookEventObj, apiKey) => {
     switch (webhookEventObj.events[0].message.type) {
         case 'location':
-            sendRequestToAPIServer(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3&count=1`)
-            .then( v => {
-                console.log(v)
+            sendRequestToAPIServer(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apiKey}&format=json&lat=${webhookEventObj.events[0].message.latitude}&lng=${webhookEventObj.events[0].message.longitude}&range=3&count=10`)
+            .then( jsonData => {
+                const jsonObj = JSON.parse(jsonData);
+                jsonObj.results.shop.forEach(element => {
+                    console.log(element.name)
+                });
             })
 
             break;
